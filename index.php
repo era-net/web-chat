16dc2d8979d016e617353891a3bc23975524b72d
era-net <erwinkevinkast@gmail.com>
2020-12-18 16:26:41 +0100

    setting things up

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="./bootstrap-4.5.3/css/bootstrap.min.css">
+
+    <title>web-chat landing page</title>
+</head>
+<body>
+<div class="container">
+    <div class="jumbotron mt-4">
+        <h1 class="display-4">Web-Chat</h1>
+        <p class="lead">Join Web-Chat and spread your wings !</p>
+        <hr class="my-4">
+        <ul class="list-group">
+            <li class="list-group-item">chat with other users about anything</li>
+            <li class="list-group-item">invite users to have a private conversation</li>
+            <li class="list-group-item">see whos active and most likely to respond fast</li>
+            <li class="list-group-item">provide your gender <i><small>(optional)</small></i></li>
+        </ul>
+
+        <div class="justify-content-end">
+            <form class="form-inline">
+                <div class="form-group mx-sm-3 mb-2">
+                    <label for="inputPassword2" class="sr-only">Password</label>
+                    <input type="password" class="form-control" id="inputPassword2" placeholder="Password">
+                </div>
+                <button type="submit" class="btn btn-primary mb-2">Confirm identity</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+    <script src="./jquery-3.5.1/jquery.min.js"></script>
+    <script src="./bootstrap-4.5.3/js/bootstrap.min.js"></script>
+</body>
+</html>
