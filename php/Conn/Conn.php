9642002cbd0cb3378aa10b17e3b2537ca835c993
era-net <erwinkevinkast@gmail.com>
2020-12-23 05:36:46 +0100

    .env environment setup

--- php/Conn/Conn.php
+++ php/Conn/Conn.php
@@ -1,9 +1,14 @@
 <?php
 
+require_once "../../vendor/autoload.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable("../../");
+$dotenv->load();
+
 $_MainDB = [
-  "server" => "",
-  "database" => "",
-  "table" => "",
-  "username" => "",
-  "password" => ""
+  "server" => $_ENV["M_server"],
+  "database" => $_ENV["M_database"],
+  "table" => $_ENV["M_table"],
+  "username" => $_ENV["M_username"],
+  "password" => $_ENV["M_password"]
 ];
