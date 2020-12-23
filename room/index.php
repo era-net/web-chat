81b9b7b021b8e0e6435237616942f1679136c1d3
era-net <erwinkevinkast@gmail.com>
2020-12-23 16:14:13 +0100

    chat room setup

--- room/index.php
+++ room/index.php
@@ -2,18 +2,25 @@
 session_start();
 
 if (isset($_SESSION["username"]) && $_SESSION["started"]) {
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../bootstrap-4.5.3/css/bootstrap.min.css">
+
     <title>Chat room</title>
 </head>
 <body>
     <a href="../quit/">quit session</a>
     <h2>Welcome to the chatting room :)</h2>
+
+    <script type="text/javascript" src="../jquery-3.5.1/jquery.min.js"></script>
+    <script type="text/javascript" src="../bootstrap-4.5.3/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="../js/xhr/xhr.min.js"></script>
+    <script type="text/javascript" src="./main.js"></script>
 </body>
 </html>
 <?php
