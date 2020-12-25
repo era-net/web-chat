6e12b4b4bd706dda911b18a0e401f89033a4861e
era-net <erwinkevinkast@gmail.com>
2020-12-25 08:47:19 +0100

    chatting room frontend

--- room/index.php
+++ room/index.php
@@ -17,6 +17,24 @@ if (isset($_SESSION["username"]) && $_SESSION["started"]) {
     <a href="../quit/">quit session</a>
     <h2>Welcome to the chatting room :)</h2>
 
+    <div class="container border-top">
+        <div class="row justify-content-md-center">
+            <div class="col col-lg-2 px-3 py-3">
+                <div class="pt-2">hehe</div>
+                <div class="pt-2">haha</div>
+                <div class="pt-2">hoho</div>
+            </div>
+            <div class="col col-lg-2 border-left pl-5 py-3">
+                <div>
+                    chat...
+                </div>
+                <div>
+                    <input type="text">
+                </div>
+            </div>
+        </div>
+    </div>
+
     <script type="text/javascript" src="../jquery-3.5.1/jquery.min.js"></script>
     <script type="text/javascript" src="../bootstrap-4.5.3/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="../js/xhr/xhr.min.js"></script>
