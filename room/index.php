232cd2fd8c8159eb5abad32a78b8d30e171cc7b1
era-net <erwinkevinkast@gmail.com>
2021-01-29 01:02:59 +0100

    chat-container styling

--- room/index.php
+++ room/index.php
@@ -25,12 +25,43 @@ if (isset($_SESSION["username"]) && $_SESSION["started"]) {
                 <div class="pt-2">haha</div>
                 <div class="pt-2">hoho</div>
             </div>
-            <div class="col col-lg-2 border-left pl-5 py-3">
-                <div class="chat-section">
-                    <div class="message">hehehehe</div>
-                    <div class="message">hehehehe</div>
-                    <div class="message">hehehehe</div>
-                    <div class="message">hehehehe</div>
+            <div class="col border-left pl-5 py-3">
+                <div class="chat-section mb-2">
+                    <div class="message-container">
+                        <div class="message-title">Marc</div>
+                        <div class="message-content">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis, eum. Cumque dolor consequatur sequi similique provident quibusdam veritatis explicabo doloremque, inventore, placeat aliquam veniam, natus ut error vero quod tempore?</div>
+                        <div class="message-info">14:22</div>
+                    </div>
+                    <div class="message-container">
+                        <div class="message-title">Flora</div>
+                        <div class="message-content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates fugiat, accusantium placeat nemo reprehenderit quae illo fugit autem aliquam sint doloribus ipsum alias amet nam deserunt error reiciendis. Deleniti, ut!</div>
+                        <div class="message-info">14:33</div>
+                    </div>
+                    <div class="message-container">
+                        <div class="message-title">Jane</div>
+                        <div class="message-content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos voluptas velit totam dolorem voluptates vel, odio rerum sequi magnam nesciunt natus deleniti pariatur culpa, odit incidunt quibusdam autem laborum. Fugiat?</div>
+                        <div class="message-info">14:45</div>
+                    </div>
+                    <div class="message-container">
+                        <div class="message-title">John</div>
+                        <div class="message-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quaerat odio recusandae temporibus aut asperiores optio atque consequuntur, dignissimos sint cumque omnis ab quam! Amet atque modi maxime veritatis cupiditate!</div>
+                        <div class="message-info">15:01</div>
+                    </div>
+                    <div class="message-container">
+                        <div class="message-title">John</div>
+                        <div class="message-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quaerat odio recusandae temporibus aut asperiores optio atque consequuntur, dignissimos sint cumque omnis ab quam! Amet atque modi maxime veritatis cupiditate!</div>
+                        <div class="message-info">15:01</div>
+                    </div>
+                    <div class="message-container">
+                        <div class="message-title">John</div>
+                        <div class="message-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quaerat odio recusandae temporibus aut asperiores optio atque consequuntur, dignissimos sint cumque omnis ab quam! Amet atque modi maxime veritatis cupiditate!</div>
+                        <div class="message-info">15:01</div>
+                    </div>
+                    <div class="message-container">
+                        <div class="message-title">John</div>
+                        <div class="message-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quaerat odio recusandae temporibus aut asperiores optio atque consequuntur, dignissimos sint cumque omnis ab quam! Amet atque modi maxime veritatis cupiditate!</div>
+                        <div class="message-info">15:01</div>
+                    </div>
                 </div>
                 <div>
                     <input type="text">
